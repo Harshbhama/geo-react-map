67864ab9a8a36140a8d127bffe43592cee31d2b5
harsh bhama <harshbhama96@gmail.com>
2019-06-20 18:07:46 +0530

    Commit

--- src/GoogleMapContainer.js
+++ src/GoogleMapContainer.js
@@ -50,20 +50,20 @@ class GoogleMapsContainer extends React.Component {
       //     { lat: 28.7041, lng: 77.1025 }, // Delhi
       //     { lat: 28.4595, lng: 77.0266 }, //Gurugram
       //   ],
-        
+
       // ],
 
-      // new_markers: [
-      //   { lat: 28.6139, lng: 77.2090 }, //Delhi
-      //   { lat: 28.7306, lng: 77.7759 }, // hapur
-      //   { lat: 28.6139, lng: 77.2090 }, //Delhi
-      //   { lat: 28.4595, lng: 77.0266 }, //Gurugram
-      //   { lat: 28.7306, lng: 77.7759 }, // Hapur
-      //   { lat: 28.7306, lng: 77.7759 }, // Gurugram
-      //   { lat: 28.7306, lng: 77.7759 }, // Hapur
-      //   { lat: 28.6692, lng: 77.4538 } //Ghaziabad
-      //  { lat: 28.4089, lng: 77.3178 } // Faridabad
-      // ],
+      new_markers: [
+        { lat: 28.6139, lng: 77.2090 }, //Delhi
+        { lat: 28.7306, lng: 77.7759 }, // hapur
+        { lat: 28.6139, lng: 77.2090 }, //Delhi
+        { lat: 28.4595, lng: 77.0266 }, //Gurugram
+        { lat: 28.7306, lng: 77.7759 }, // Hapur
+        { lat: 28.7306, lng: 77.7759 }, // Gurugram
+        { lat: 28.7306, lng: 77.7759 }, // Hapur
+        { lat: 28.6692, lng: 77.4538 }, //Ghaziabad
+        { lat: 28.4089, lng: 77.3178 } // Faridabad
+      ],
       address: ''
     }
 
@@ -71,7 +71,7 @@ class GoogleMapsContainer extends React.Component {
     let newData = []
     let i = 0;
     let company;
-    Geocode.setApiKey("x");
+    Geocode.setApiKey("AIzaSyDa8IbCs7c-NM1Yw0eoXAqvxFJ27aQ6s58");
 
     // binding this to event-handler functions
     this.onMarkerClick = this.onMarkerClick.bind(this);
@@ -79,66 +79,85 @@ class GoogleMapsContainer extends React.Component {
   }
 
   componentWillMount() {
-    //this.getData()
+    this.getData()
     const abc = this
-    let marker1 =  [
-      { lat: 28.7041, lng: 77.1025 }, // Delhi
-      { lat: 28.7306, lng: 77.7759 }, // Hapur
-    ]
-    let marker2 = [
-      { lat: 28.7041, lng: 77.1025 }, // Delhi
-      { lat: 28.4595, lng: 77.0266 }, //Gurugram
-    ]
-    let marker3 = [
-      { lat: 28.7306, lng: 77.7759 }, // Hapur
-      { lat: 28.4089, lng: 77.3178 }, // faridabad
-    ]
-    let marker4 = [
-      { lat: 28.7306, lng: 77.7759 }, // Hapur
-      { lat: 28.4215, lng: 78.0195 }, // Bulandsher
-    ]
-    let demo_marker1 = [marker1, marker2, marker3, marker4]
-    console.log(demo_marker1)
-    abc.setState({
-      demo_marker1: demo_marker1
-    })
+    // let marker1 = [
+    //   { lat: 28.7041, lng: 77.1025 }, // Delhi
+    //   { lat: 28.7306, lng: 77.7759 }, // Hapur
+    // ]
+    // let marker2 = [
+    //   { lat: 28.7041, lng: 77.1025 }, // Delhi
+    //   { lat: 28.4595, lng: 77.0266 }, //Gurugram
+    // ]
+    // let marker3 = [
+    //   { lat: 28.7306, lng: 77.7759 }, // Hapur
+    //   { lat: 28.4089, lng: 77.3178 }, // faridabad
+    // ]
+    // let marker4 = [
+    //   { lat: 28.7306, lng: 77.7759 }, // Hapur
+    //   { lat: 28.4215, lng: 78.0195 }, // Bulandsher
+    // ]
+    // let demo_marker1 = [marker1, marker2, marker3, marker4]
+    // console.log(demo_marker1)
+    // abc.setState({
+    //   demo_marker1: demo_marker1
+    // })
   }
 
   getData() {
-    // const abc = this
-    // let initData = {}
-    // let newData = []
-    // let i = 0;
-    // let company;
-    // let k;
-    // this.state.data.value.shipment.forEach(function (a, index) {
-    //   let array = { ...initData }
-    //   let p = {}
-    //   debugger
-    //   if (index + 2 < abc.state.data.value.shipment.length) {
+    const abc = this
+    let initData = {}
+    let newData = []
+    let demoo = []
+    let i = 0;
+    let j = 0;
+    let company;
+    let k;
+    this.state.data.value.shipment.forEach(function (a, index) {
+      let array = { ...initData }
+      let p = {}
+      debugger
+      if (index + 2 < abc.state.data.value.shipment.length) {
 
-    //     if (abc.state.data.value.shipment[index].seller_company_type_name === abc.state.data.value.shipment[index + 1].seller_company_type_name || abc.state.data.value.shipment[index].seller_company_type_name === abc.state.data.value.shipment[index + 2].seller_company_type_name) {
-    //       array = { lat: abc.state.data.value.shipment[index - 1].lat, lng: abc.state.data.value.shipment[index - 1].lng }
-    //       p = array
-    //       newData[i++] = array
-    //       company = abc.state.data.value.shipment[index].seller_company_type_name
-    //     }
+        // if (abc.state.data.value.shipment[index].seller_company_type_name === abc.state.data.value.shipment[index + 1].seller_company_type_name || abc.state.data.value.shipment[index].seller_company_type_name === abc.state.data.value.shipment[index + 2].seller_company_type_name) {
+        //   array = { lat: abc.state.data.value.shipment[index - 1].lat, lng: abc.state.data.value.shipment[index - 1].lng }
+        //   p = array
+        //   newData[i++] = array
+        //   company = abc.state.data.value.shipment[index].seller_company_type_name
+        // }
 
 
-    //     if (abc.state.data.value.shipment[index].seller_company_type_name === company) {
-    //       array = { lat: abc.state.data.value.shipment[index].lat, lng: abc.state.data.value.shipment[index].lng }
-    //       newData[i++] = array
-    //       newData[i++] = p
-    //       index++
-    //     }
-    //   }
-    //   array = { lat: abc.state.data.value.shipment[index].lat, lng: abc.state.data.value.shipment[index].lng }
-    //   newData[i++] = array
-    // })
+        // if (abc.state.data.value.shipment[index].seller_company_type_name === company) {
+        //   array = { lat: abc.state.data.value.shipment[index].lat, lng: abc.state.data.value.shipment[index].lng }
+        //   newData[i++] = array
+        //   newData[i++] = p
+        //   index++
+        // }
+        newData[i++] = a.marker
+        console.log(a.marker)
 
-    // console.log(newData)
+        a.marker.forEach((fff, index) => {
+          demoo[j++] = fff
+        })
+
+
+
+      }
+      // array = { lat: abc.state.data.value.shipment[index].lat, lng: abc.state.data.value.shipment[index].lng }
+      // newData[i++] = array
+      // demoo.push(newData)
+      // console.log(demoo)
+    })
+
+    console.log(newData)
+    console.log(demoo)
+    // while (newData.length) {
+    //   demoo[j++] = newData.splice(0, 2)
+    // }
+
+    // console.log(demoo)
     // abc.setState({
-    //   testData: newData
+    //   testData: demoo
     // })
 
 
@@ -180,6 +199,7 @@ class GoogleMapsContainer extends React.Component {
     const google = window.google;
     let bounds = new google.maps.LatLngBounds();
     console.log(this.state.demo_marker1)
+    console.log(this.state.data.value.demo_marker)
     return (
       <Map
         item
@@ -190,7 +210,7 @@ class GoogleMapsContainer extends React.Component {
         zoom={10}
         initialCenter={{ lat: 28.6139, lng: 77.2090 }}
       >
-        {this.state.marker.map(marker => (
+        {this.state.data.value.shipment.map(marker => (
           <Marker
             onClick={this.onMarkerClick}
             title={'Changing Colors Garage'}
@@ -213,9 +233,9 @@ class GoogleMapsContainer extends React.Component {
         >
           {this.state.address}
         </InfoWindow>
-        {console.log(this.state.data.value.shipment)}
+        {console.log(this.state.data.value.demo_marker)}
 
-        {this.state.demo_marker1.map((marker, index) => (
+        {this.state.data.value.demo_marker.map((marker, index) => (
           <Polyline
             path={marker}
             geodesic={true}
@@ -256,5 +276,5 @@ class GoogleMapsContainer extends React.Component {
   }
 }
 export default GoogleApiWrapper({
-  apiKey: 'x'
+  apiKey: 'AIzaSyDa8IbCs7c-NM1Yw0eoXAqvxFJ27aQ6s58'
 })(GoogleMapsContainer)
